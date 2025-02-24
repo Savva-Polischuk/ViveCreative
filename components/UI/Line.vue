fa6c1ab2a2075633b5e44c264648ee9497408788
Savva Polischuk <savva.polishuk@gmail.com>
2025-02-24 23:04:39 +0300

    S-dev: новый компонент разделительной линии

---
+++ components/UI/Line.vue
@@ -0,0 +1,37 @@
+<template lang='pug'>
+.Line
+</template>
+
+<script lang='ts' setup>
+interface Props {
+	isVertical?: boolean // true - вертикальная
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style lang='sass' scoped>
+.Line
+    border-top: 1px solid #fff
+    width: 100%
+    height: 0
+
+    &.vertical
+        border-right: 1px solid #fff
+        width: 0
+        height: 100%
+
+    &.gr-to-dark
+        border: 1px solid transparent
+        border-image: linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, .5))
+        -moz-border-image: -moz-linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, .5))
+        -webkit-border-image: -webkit-linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, .5))
+        border-image-slice: 1
+    
+    &.gr-to-light
+        border: 1px solid transparent
+        border-image: linear-gradient(to right, rgba(255, 255, 255, .5), rgba(255, 255, 255, 1))
+        -moz-border-image: -moz-linear-gradient(to right, rgba(255, 255, 255, .5), rgba(255, 255, 255, 1))
+        -webkit-border-image: -webkit-linear-gradient(to right, rgba(255, 255, 255, .5), rgba(255, 255, 255, 1))
+        border-image-slice: 1
+</style>

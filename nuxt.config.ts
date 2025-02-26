3b16e24ad97b28cfcbab2d6a5ca3e5969b1cb160
Savva Polischuk <savva.polishuk@gmail.com>
2025-02-26 14:41:22 +0300

    S-dev-icon: Создан компонент иконки IconImg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  modules: [
+    '@nuxt/icon'
+  ],
+  icon: {
+    customCollections: [
+      {
+        prefix: 'icon',
+        dir: './assets/icons',
+      },
+    ],
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/sass/main.sass'],
